dd427a814d9a159515eac7a42aa0322c31f85207
Daniel <ct514@come.tech.com.tw>
2025-01-15 15:57:35 +0800

    1. 新增終局版 2. 新增滿疊金幣的顯示階級 3. 現在終局版會隱藏1000以下的金幣、速刷版會隱藏300以下的金幣 4. 現在終局版會隱藏工匠碎片、點金石碎片、富豪石碎片、蛻變石、增幅石、鍛鐵符文 5. 新增幻象異界、裂痕石、謁見國王 6. 新增高價預兆 7. 修正預兆不會正確顯示的問題 8. 減少巨靈之幣、最後通牒雕刻的顯示大小 9. 新增特殊巨靈之幣 10. 新增稀有傳奇的基底過濾(珠寶、香爐聖物(終焉烈焰)|器皿聖物(第二聖事)、藍玉戒指(夢語之痕)|恆星項鍊(均衡之符)|重革腰帶(獵首)|實用腰帶(獨創)|終極生命藥劑(奧爾羅斯的決心)|終極魔力藥劑(融解漩渦)|戒指(卡蘭德之觸)|隱秘裹手(智行之手))

--- AutoUpdateFilter.ps1
+++ AutoUpdateFilter.ps1
@@ -3,10 +3,10 @@ $content = Get-Content "POE2_Moon.filter"
 
 # 替換內容
 $content1 = $content -replace 'Show #印鈔 - 終局 - 速刷', 'Hide #印鈔 - 終局 - 速刷'
-# $content2 = $content -replace 'Show #印鈔 - 終局', 'Hide #印鈔 - 終局'
+$content2 = $content -replace 'Show #印鈔 - 終局', 'Hide #印鈔 - 終局'
 # $content3 = $content -replace 'Show #印鈔', 'Hide #印鈔'
 
 # 將修改後的內容儲存為新檔案
 $content1 | Set-Content "POE2_Moon_1_速刷.filter"
-# $content2 | Set-Content "POE2_Moon_2_終局.filter"
+$content2 | Set-Content "POE2_Moon_2_終局.filter"
 # $content3 | Set-Content "POE2_Moon_3_印鈔.filter"
