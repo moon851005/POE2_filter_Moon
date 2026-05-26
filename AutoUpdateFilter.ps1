cb8dbddc668d2eea0db011aaaea09fa1961d97ac
moon851005 <lelouch80548@gmail.com>
2026-05-26 21:23:47 +0800

    1. 過濾器分級標籤更新

--- AutoUpdateFilter.ps1
+++ AutoUpdateFilter.ps1
@@ -1,10 +1,10 @@
 # 讀取原檔案內容
-$content = Get-Content "POE2_Moon.filter"
+$content0 = Get-Content "POE2_Moon.filter"
 
 # 替換內容
-$content1 = $content -replace 'Show #印鈔 - 終局 - 速刷', 'Hide #印鈔 - 終局 - 速刷'
-$content2 = $content -replace 'Show #印鈔 - 終局', 'Hide #印鈔 - 終局'
-$content3 = $content -replace 'Show #印鈔', 'Hide #印鈔'
+$content1 = $content0 -replace 'Show # %H0', 'Hide # %H0' #速刷
+$content2 = $content1 -replace 'Show # %H1', 'Hide # %H1' #終局
+$content3 = $content2 -replace 'Show # %H2', 'Hide # %H2' #印鈔
 
 # 將修改後的內容儲存為新檔案
 $content1 | Set-Content "POE2_Moon_1_速刷.filter"
